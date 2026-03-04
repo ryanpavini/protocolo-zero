ddc915a9e0783e58954347f20eb2708b40682527
Ryan Pavini <r236799@dac.unicamp.br>
2026-03-04 12:21:00 -0300

    Initial commit: Spring Boot skeleton generated

---
+++ src/main/java/com/ozymandias/protocolozero/ProtocoloZeroApplication.java
@@ -0,0 +1,13 @@
+package com.ozymandias.protocolozero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtocoloZeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtocoloZeroApplication.class, args);
+	}
+
+}
